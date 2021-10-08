7bf48218fc028448d37d72ab06b0a8657d4085a7
Your Name <you@example.com>
2021-10-08 13:49:49 +0000

    Added new Tweet class

--- twitter/tweet.py
+++ twitter/tweet.py
@@ -2,14 +2,11 @@ from .user import User
 from typing import Optional, Dict, Any
 
 class Tweet:
-    def __init__(self, client, data: Dict[str, Any]):
-        self.client = client
-        print(data)
+    def __init__(self, data: Dict[str, Any]):
+        self.original_payload = data
         self._payload = data['data']
     
     @property
     def author(self) -> Optional[User]:
-        return self.client.get_user_by_id(int(self._payload.get("id")))
+        return User(self.original_payload["includes"]["users"][0])
 
-class ReTweet(Tweet):
-    ...
