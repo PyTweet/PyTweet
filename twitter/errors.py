df1c7cd8fe5ac9713022536ecbc7889a5fd85152
TheKungFuChuck <>
2021-10-09 14:35:57 +0000

    Added more error class

--- twitter/errors.py
+++ twitter/errors.py
@@ -1,7 +1,12 @@
 class Unauthorized(Exception):
-    "Raised when the api-key is invalid!"
+    """Raised when the api-key is invalid!"""
     pass
 
-class UserNotFound(Exception):
-    "Raised when it cant find the right user in get_user functions"
+class NotFoundError(Exception):
+    """Raised when it cant find the object in some functions."""
     pass
+
+class MissingCredentials(Exception):
+    """Raised upon using function that needed certain Credentials like consumer_key or consumer_key_secret"""
+    pass
+
