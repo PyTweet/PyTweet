c5ad78287acc2815b4f72d0b73045ca0ca24561b
TheKungFuChuck <>
2021-10-14 04:59:09 +0000

    Fixed error handler.

--- twitter/http.py
+++ twitter/http.py
@@ -23,18 +23,23 @@ SOFTWARE.
 """
 
 import requests
-import json
 from typing import Dict, Any, Optional
 from .errors import Unauthorized, NotFoundError 
+from requests import Session
+
+def is_error(respond: requests.models.Response):
+    code=respond.status_code
+    if code == 401:
+        raise Unauthorized("Invalid credentials passed!")
 
 class Route:
     def __init__(self, method:str, version: str, path:str):
-        url=f"https://api.twitter.com/{version}"
         self.method: str = method
         self.path: str = path
-        self.url: str = url + self.path
+        self.base_url=f"https://api.twitter.com/{version}"
+        self.url: str = self.base_url + self.path
 
-class HTTPClient:
+class HTTPClient():
     """
     Represent the http/base client for :class: Client! 
     This http/base client will be the parent class for :class: Client.
@@ -54,8 +59,6 @@ class HTTPClient:
     Functions:
     ====================
     def request() -> make a requests with the given paramaters.
-
-    def is_error() -> Check if a requests return error code: 401
     """
     def __init__(self, bearer_token:str, *, consumer_key=Optional[str], consumer_key_secret=Optional[str], access_token=Optional[str], access_token_secret=Optional[str]):
         self.bearer_token = bearer_token
@@ -65,7 +68,7 @@ class HTTPClient:
         self.access_token_secret = access_token_secret 
         
     
-    def request(self, route:Route, *,payload:Dict[str, Any], params:Dict[str, str], is_json: bool = True) -> Any:
+    def request(self, route:Route, *,headers:Dict[str, Any], params:Dict[str, str], is_json: bool = True) -> Any:
         method=getattr(route, 'method', None)
         if not method:
             raise TypeError("Method isnt recognizable")
@@ -74,34 +77,17 @@ class HTTPClient:
         if not res:
             raise TypeError("Method isnt recognizable")
         
-        respond=res(route.url, headers=payload, params=params)
+        respond=res(route.url, headers=headers, params=params)
+        is_error(respond)
+        res=respond.json()
         
-        self.is_error(respond)
-        
-        if 'data' not in respond.text:
-            error=json.loads(respond.text)["errors"][0]
-            try:
-                error["detail"]
-            except KeyError:
-                raise Exception(error)
-            else:
-                raise NotFoundError(error["detail"])
+        if 'errors' in res.keys():
+            raise NotFoundError(res["errors"][0]["detail"]) 
 
-            
+        elif 'meta' in res.keys(): 
+            if res["meta"]["result_count"] == 0:
+                return 0
+        
         if is_json:
-            return respond.json()['data']
-        return respond
-    
-    def bearer_oauth(self, r): #Taken from sample code
-        """
-        Method required by bearer token authentication.
-        """
-
-        r.headers["Authorization"] = f"Bearer {self.bearer_token}"
-        r.headers["User-Agent"] = "v2TweetLookupPython"
-        return r 
-
-    def is_error(self, respond: requests.models.Response):
-        code=respond.status_code
-        if code == 401:
-            raise Unauthorized("Invalid bearer_token passed!")
+            return res['data']
+        return res
