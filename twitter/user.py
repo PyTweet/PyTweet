e1c28895c262e07825b3a14bb1f3f6dc01f18220
TheKungFuChuck <>
2021-10-17 14:52:25 +0000

    Black formatted.

--- twitter/user.py
+++ twitter/user.py
@@ -28,9 +28,10 @@ from dateutil import parser
 from .abc import Messageable
 from .http import HTTPClient
 
+
 class UserPublicMetrics:
     """
-    Represent a PublicMetrics for a User. 
+    Represent a PublicMetrics for a User.
     This PublicMetrics contain public info about the user.
 
     Parameters:
@@ -45,21 +46,22 @@ class UserPublicMetrics:
 
     :property: following_count -> Returns total of following that a user has.
     """
+
     def __init__(self, data=Dict[str, Any], **kwargs):
-        self.original_payload=data
-        self._public=data.get('public_metrics')
+        self.original_payload = data
+        self._public = data.get("public_metrics")
 
     @property
     def followers_count(self) -> int:
-        return self._public.get('followers_count')
+        return self._public.get("followers_count")
 
     @property
     def following_count(self) -> int:
-        return self._public.get('following_count')
+        return self._public.get("following_count")
 
     @property
     def tweet_count(self) -> int:
-        return self._public.get('tweet_count')
+        return self._public.get("tweet_count")
 
     def __str__(self) -> str:
         return f"<UserPublicMetrics: user={self.original_payload.get('username')} followers_count={self._payload.get('followers_count')} following_count={self._payload.get('following_count')} tweet_count={self._payload.get('tweet_count')}>"
@@ -67,9 +69,10 @@ class UserPublicMetrics:
     def __repr__(self) -> str:
         return f"<UserPublicMetrics: User={self.original_payload.get('username')}>"
 
+
 class User(UserPublicMetrics, Messageable):
     """
-    Represent a user in Twitter. 
+    Represent a user in Twitter.
     This user is an account that has created by other person, not from an apps.
 
     Parameters:
@@ -85,7 +88,7 @@ class User(UserPublicMetrics, Messageable):
     :property: description -> Return the user's description.
 
     :property: link -> Return url where the user put in links, return None if there isnt a url.
-    
+
     :property: id -> Return the user's id.
 
     :property: verified -> Return True if the user is verified account, else False.
@@ -96,73 +99,83 @@ class User(UserPublicMetrics, Messageable):
 
     :property: created_at -> Return datetime.datetime object with user's account date.
 
-    :property: location -> Return a user's location, Somehow it return None in get_user_by_username and get_user function, Get it using get_tweet function. Will fix that soon!  
+    :property: location -> Return a user's location, Somehow it return None in get_user_by_username and get_user function, Get it using get_tweet function. Will fix that soon!
 
     :property: tweet_count -> Return total tweet that the user tweeted.
 
     :property: followers -> Returns a list of users who are followers of the specified user ID.
 
-    :property: following -> Returns a list of users thats followed by the specified user ID.  
+    :property: following -> Returns a list of users thats followed by the specified user ID.
     """
-    def __init__(self, data:Dict[str, Any], **kwargs): 
+
+    def __init__(self, data: Dict[str, Any], **kwargs):
         super().__init__(data, **kwargs)
-        self._payload=data
-        self.http_client: Optional[HTTPClient] = kwargs.get('http_client') or None
-        self.description=self.bio
+        self._payload = data
+        self.http_client: Optional[HTTPClient] = kwargs.get("http_client") or None
+        self.description = self.bio
 
     def __str__(self) -> str:
-        return "<User: name={0.name} username={0.username} description={0.description} id={0.id} created_at={0.created_at} verified={0.verified} protected={0.protected} avatar_url={0.avatar_url} location={0.location} followers_count={0.followers_count} following_count={0.following_count} tweet_count={0.tweet_count}>".format(self)
+        return "<User: name={0.name} username={0.username} description={0.description} id={0.id} created_at={0.created_at} verified={0.verified} protected={0.protected} avatar_url={0.avatar_url} location={0.location} followers_count={0.followers_count} following_count={0.following_count} tweet_count={0.tweet_count}>".format(
+            self
+        )
 
     def __repr__(self) -> str:
-        return "<User Object: {0.username}>".format(self) 
+        return "<User Object: {0.username}>".format(self)
 
     @property
     def name(self) -> str:
-        return self._payload.get('name')
+        return self._payload.get("name")
 
     @property
     def username(self) -> str:
-        return "@" + self._payload.get('username')
-    
+        return "@" + self._payload.get("username")
+
     @property
     def id(self) -> int:
-        return self._payload.get('id')
+        return self._payload.get("id")
 
     @property
     def bio(self) -> str:
-        return self._payload.get('description')
-   
+        return self._payload.get("description")
+
     @property
     def profile_link(self) -> str:
-        return f"https://twitter.com/{self.username.replace('@', '')}" 
+        return f"https://twitter.com/{self.username.replace('@', '')}"
 
     @property
     def link(self) -> str:
-        return self._payload.get('url')
+        return self._payload.get("url")
 
     @property
     def verified(self) -> bool:
-        return self._payload.get('verified')
+        return self._payload.get("verified")
 
     @property
     def protected(self) -> bool:
-        return self._payload.get('protected')
+        return self._payload.get("protected")
 
     @property
     def avatar_url(self) -> Optional[str]:
-        return self._payload.get('profile_image_url')
+        return self._payload.get("profile_image_url")
 
     @property
     def location(self) -> Optional[str]:
-        return self._payload.get('location')
+        return self._payload.get("location")
 
     @property
     def created_at(self) -> datetime.datetime:
-        date=str(parser.parse(self._payload.get('created_at')))
-        y, mo, d=date.split("-")
-        h, mi, s=date.split(" ")[1].split('+')[0].split(":")
-        
-        return datetime.datetime(year=int(y), month=int(mo), day=int(d.split(" ")[0]), hour=int(h), minute=int(mi), second=int(s))
+        date = str(parser.parse(self._payload.get("created_at")))
+        y, mo, d = date.split("-")
+        h, mi, s = date.split(" ")[1].split("+")[0].split(":")
+
+        return datetime.datetime(
+            year=int(y),
+            month=int(mo),
+            day=int(d.split(" ")[0]),
+            hour=int(h),
+            minute=int(mi),
+            second=int(s),
+        )
 
     @property
     def followers(self) -> List[object]:
