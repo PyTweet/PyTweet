2e8b4190cf17fa84c79a2616ac495de9ccf573b9
TheKungFuChuck <>
2021-10-21 07:33:49 +0000

    .

--- twitter/__init__.py
+++ twitter/__init__.py
@@ -9,5 +9,8 @@ from .user import *
 
 
 
+__title__ = "PyTweet"
 __version__ = "1.1.0"
 __authors__ = ["TheFarGG", "TheGenocides"]
+__license__ = "MIT"
+__copyright__ = "Copyright 2021 TheFarGG & TheGenocides"
