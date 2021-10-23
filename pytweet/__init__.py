f9640e7f6e05d4506a52d34a11ce55c0f8b937b6
TheKungFuChuck <>
2021-10-23 05:42:40 +0000

    Bump Version

---
+++ pytweet/__init__.py
@@ -0,0 +1,16 @@
+from .abc import *
+from .attachments import *
+from .client import *
+from .errors import *
+from .http import *
+from .metrics import *
+from .tweet import *
+from .user import *
+
+
+
+__title__ = "PyTweet"
+__version__ = "1.1.2"
+__authors__ = ["TheFarGG", "TheGenocides"]
+__license__ = "MIT"
+__copyright__ = "Copyright 2021 TheFarGG & TheGenocides"
