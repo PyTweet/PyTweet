f1c3ed0b45a3eb7b30ab19b82f786464e6b3ed46
TheKungFuChuck <>
2021-10-29 09:09:46 +0000

    Added MessageTypeEnum enums

--- pytweet/enums.py
+++ pytweet/enums.py
@@ -1,7 +1,10 @@
 from enum import Enum
 
-
 class RelationsTypeEnum(Enum):
     PENDING = 0
     ACCEPT = 1
     NUL = None
+
+class MessageTypeEnum(Enum):
+    DIRECT_MESSAGE = 0
+    MESSAGE_TWEET = 1
