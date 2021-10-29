e76deffc3177d6530f9e6a1e21c4e0f0d8160489
TheKungFuChuck <>
2021-10-29 13:55:41 +0000

    Remove useless space

--- pytweet/auth.py
+++ pytweet/auth.py
@@ -1,6 +1,5 @@
 from requests_oauthlib import OAuth1, OAuth1Session
 
-
 class OauthSession(OAuth1Session):
     def __init__(self, consumer_key: str, consumer_secret: str, callback=None):
         super().__init__(consumer_key, client_secret=consumer_secret, callback_uri=callback)
