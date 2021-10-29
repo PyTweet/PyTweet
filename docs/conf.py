5ff3b367847571f14f7fbad34054981a13e870ca
Sengolda <senushkabatawala@gmail.com>
2021-10-29 10:00:04 +0530

    uh

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_material'
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
