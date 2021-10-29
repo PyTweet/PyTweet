b335432ade72f179b352eb655497ff3a56dbe0e2
Sengolda <senushkabatawala@gmail.com>
2021-10-30 02:02:38 +0530

    Added doc to __init__.py

--- pytweet/__init__.py
+++ pytweet/__init__.py
@@ -1,3 +1,12 @@
+"""
+PyTweet
+~~~~~~~
+
+PyTweet is a Synchronous python API wrapper for Twitter's API!
+
+:copyright: (c) 2021 TheFarGG & TheGenocides
+:license: MIT, see LICENSE for more details.
+"""
 from .attachments import *
 from .auth import *
 from .client import *
