f2f68759cce345fa90975b5d819e306e4fcc18a7
TheKungFuChuck <>
2021-11-03 14:10:13 +0000

    Added NULL to enums

--- pytweet/enums.py
+++ pytweet/enums.py
@@ -1,17 +1,16 @@
 from enum import Enum
 
-
 class RelationsTypeEnum(Enum):
     PENDING = 0
     ACCEPT = 1
-    NUL = None
+    NULL = None
 
 class MessageTypeEnum(Enum):
     DIRECT_MESSAGE = 0
     MESSAGE_TWEET = 1
     NULL = None
 
-class MessageEventsTypeEnum(Enum):
+class MessageEventTypeEnum(Enum):
     MESSAGE_CREATE = "message_create"
     MESSAGE_DELETE = "message_delete"
     NULL = None
