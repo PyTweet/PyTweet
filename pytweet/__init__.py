ec26bedd2c9f1b36752e5582ae462d21ef50d189
Sengolda <senushkabatawala@gmail.com>
2021-11-05 17:47:13 +0530

    Versioninfo and CLI

--- pytweet/__init__.py
+++ pytweet/__init__.py
@@ -7,6 +7,7 @@ PyTweet is a Synchronous python API wrapper for Twitter's API!
 :copyright: (c) 2021 TheFarGG & TheGenocides
 :license: MIT, see LICENSE for more details.
 """
+from typing import Literal, NamedTuple
 import logging
 
 from .attachments import *
@@ -27,5 +28,14 @@ __version__ = "1.2.0"
 __authors__ = ["TheFarGG", "TheGenocides"]
 __license__ = "MIT"
 __copyright__ = "Copyright 2021 TheFarGG & TheGenocides"
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(major=1, minor=2, micro=0, releaselevel="final", serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
---
+++ pytweet/__main__.py
@@ -0,0 +1,44 @@
+import argparse
+import sys
+from pathlib import Path
+
+import pytweet
+import pkg_resources
+import requests
+import platform
+
+
+def show_version():
+    entries = []
+
+    entries.append("- Python v{0.major}.{0.minor}.{0.micro}-{0.releaselevel}".format(sys.version_info))
+    entries.append("- pytweet v{0.major}.{0.minor}.{0.micro}-{0.releaselevel}".format(pytweet.version_info))
+    entries.append(f"- requests v{requests.__version__}")
+    uname = platform.uname()
+    entries.append("- system info: {0.system} {0.release} {0.version}".format(uname))
+    print("\n".join(entries))
+
+
+def core(args):
+    if args.version:
+        show_version()
+    else:
+        print(
+            "Hi Thank you for using pytweet! You can do can do `python3 -m pytweet --version` for version info!\n\nDocs: https://py-tweet.readthedocs.io/ \nGithub: https://github.com/TheFarGG/PyTweet/"
+        )
+
+
+def parse_args():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("-v", "--version", action="store_true", help="shows the library versioninfo")
+    argparser.set_defaults(func=core)
+    return argparser.parse_args()
+
+
+def main():
+    args = parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
