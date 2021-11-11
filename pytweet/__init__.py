f585fa7e213786af9c83d6fe7b1616ca173dcfb4
TheKungFuChuck <>
2021-11-11 14:55:16 +0000

    Bump version to 1.3.5.a0

--- pytweet/__init__.py
+++ pytweet/__init__.py
@@ -39,6 +39,6 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=1, minor=2, micro=5, releaselevel="final", serial=0)
+version_info: VersionInfo = VersionInfo(major=1, minor=3, micro=5, releaselevel="alpha", serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
