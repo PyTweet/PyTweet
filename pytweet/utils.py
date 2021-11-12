27a26c0c5691e39bdafb8fdd0b84ced44639653f
TheKungFuChuck <>
2021-11-12 15:48:38 +0000

    Accidently delete utils.py

--- pytweet/utils.py
+++ pytweet/utils.py
@@ -5,7 +5,6 @@ from dateutil import parser
 
 def time_parse_todt(date: Optional[Any]) -> datetime.datetime:
     """:class:`datetime.datetime`: Parse time return from twitter to datetime object!
-
     .. versionadded: 1.1.3
     """
     date = str(parser.parse(date))
@@ -21,10 +20,8 @@ def time_parse_todt(date: Optional[Any]) -> datetime.datetime:
         second=int(s),
     )
 
-
 def compose_tweet() -> str:
     """:class:`str`: Make a link that lets you compose a tweet
-
     .. versionadded: 1.3.5
     """
     return "https://twitter.com/intent/tweet"
@@ -32,7 +29,6 @@ def compose_tweet() -> str:
 
 def showcase_user(username: str):
     """:class:`str`: Make a link that lets you showcase user.
-
     .. versionadded: 1.3.5
     """
     return f"https://twitter.com/{username}"
@@ -40,7 +36,6 @@ def showcase_user(username: str):
 
 def compose_tweet_action(tweet_id: Union[str, int], action: str):
     """:class:`str`: Make a link that lets you interact a tweet with certain actions.
-
     .. versionadded: 1.3.5
     """
     actions = ["retweet", "like", "reply"]
