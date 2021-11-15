315a3ab08cf115141f5e9939425d593322704cf2
TheKungFuChuck <>
2021-11-15 10:56:20 +0000

    Reformat with black

--- pytweet/user.py
+++ pytweet/user.py
@@ -16,18 +16,12 @@ if TYPE_CHECKING:
 class User:
     """Represent a user in Twitter.
     User is an identity in twitter, its very interactive. Can send message, post a tweet, and even send messages to other user through Dms.
-
     .. describe:: x == y
         Check if one user id is equal to another.
-
-
     .. describe:: x != y
         Check if one user id is not equal to another.
-
-
     .. describe:: str(x)
         Get the user's name.
-
     .. versionadded: 1.0.0
     """
 
@@ -55,7 +49,6 @@ class User:
 
     def send(self, text: str, *, quick_reply: QuickReply = None, cta: CTA = None) -> DirectMessage:
         """Send a message to the user.
-
         Parameters
         ------------
         text: :class:`str`
@@ -64,13 +57,10 @@ class User:
             The QuickReply attachment that will be send to a user.
         cta: :class:`CTA`
             cta or call-to-actions is use to make an action whenever a user 'call' something, a quick example is buttons.
-
         Returns
         ---------
         :class:`DirectMessage`
             This method return a :class:`DirectMessage` object.
-
-
         .. versionadded:: 1.1.0
         """
         return self.http_client.send_message(
@@ -83,18 +73,14 @@ class User:
 
     def follow(self) -> RelationFollow:
         """Make a Request to follow a User.
-
         Parameters
         ------------
         user_id: Union[:class:`str`, :class:`int`]
             The user's id that you wish to follow.
-
         Returns
         ---------
         :class:`RelationFollow`
             This method return a :class:`RelationFollow` object.
-
-
         .. versionadded:: 1.1.0
         """
         my_id = self.http_client.access_token.partition("-")[0]
@@ -109,18 +95,14 @@ class User:
 
     def unfollow(self) -> RelationFollow:
         """Make a DELETE Request to unfollow a User.
-
         Parameters
         ------------
         user_id: Union[:class:`str`, :class:`int`]
             The user's id that you wish to unfollow.
-
         Returns
         ---------
         :class:`RelationFollow`
             This method return a :class:`RelationFollow` object.
-
-
         .. versionadded:: 1.1.0
         """
         my_id = self.http_clientaccess_token.partition("-")[0]
@@ -129,13 +111,10 @@ class User:
 
     def block(self) -> None:
         """Make a POST Request to Block a User.
-
         Parameters
         ------------
         user_id: Union[:class:`str`, :class:`int`]
             The user's id that you wish to block.
-
-
         .. versionadded:: 1.2.0
         """
         my_id = self.http_client.access_token.partition("-")[0]
@@ -149,13 +128,10 @@ class User:
 
     def unblock(self) -> None:
         """Make a DELETE Request to unblock a User.
-
         Parameters
         ------------
         user_id: Union[:class:`str`, :class:`int`]
             The user's id that you wish to unblock.
-
-
         .. versionadded:: 1.2.0
         """
         my_id = self.http_client.access_token.partition("-")[0]
@@ -163,13 +139,10 @@ class User:
 
     def mute(self) -> None:
         """Make a POST Request to mute a User.
-
         Parameters
         ------------
         user_id: Union[:class:`str`, :class:`int`]
             The user's id that you wish to mute.
-
-
         .. versionadded:: 1.2.5
         """
         my_id = self.http_client.access_token.partition("-")[0]
@@ -183,13 +156,10 @@ class User:
 
     def unmute(self) -> None:
         """Make a DELETE Request to unmute the User.
-
         Parameters
         ------------
         user_id: Union[:class:`str`, :class:`int`]
             The user's id that you wish to unmute.
-
-
         .. versionadded:: 1.2.5
         """
         my_id = self.http_client.access_token.partition("-")[0]
@@ -197,7 +167,6 @@ class User:
 
     def trigger_typing(self):
         """Indicates that the client is typing in a user Dm.
-
         .. versionadded:: 1.3.5
         """
         self.http_client.request(
@@ -215,7 +184,6 @@ class User:
         -----------
         block:
             Indicates that the client perform a block action to the user if set to True. Default to True.
-
         .. versionadded:: 1.3.5
         """
         self.http_client.request(
@@ -228,7 +196,6 @@ class User:
     @property
     def name(self) -> str:
         """:class:`str`: Return the user's name.
-
         .. versionadded: 1.0.0
         """
         return self._payload.get("name")
@@ -236,7 +203,6 @@ class User:
     @property
     def username(self) -> str:
         """:class:`str`: Return the user's username, this usually start with '@' follow by their username.
-
         .. versionadded: 1.0.0
         """
         return "@" + self._payload.get("username")
@@ -244,7 +210,6 @@ class User:
     @property
     def id(self) -> int:
         """:class:`int`: Return the user's id.
-
         .. versionadded: 1.0.0
         """
         return int(self._payload.get("id"))
@@ -252,7 +217,6 @@ class User:
     @property
     def bio(self) -> str:
         """:class:`str`: Return the user's bio.
-
         .. versionadded: 1.0.0
         """
         return self._payload.get("description")
@@ -260,7 +224,6 @@ class User:
     @property
     def description(self) -> str:
         """:class:`str`: an alias to User.bio.
-
         .. versionadded: 1.0.0
         """
         return self._payload.get("description")
@@ -268,7 +231,6 @@ class User:
     @property
     def profile_link(self) -> str:
         """:class:`str`: Return the user's profile link
-
         .. versionadded: 1.0.0
         """
         return f"https://twitter.com/{self.username.replace('@', '', 1)}"
@@ -276,7 +238,6 @@ class User:
     @property
     def link(self) -> str:
         """:class:`str`: Return url where the user put links, return an empty string if there isn't a url
-
         .. versionadded: 1.0.0
         """
         return self._payload.get("url")
@@ -284,7 +245,6 @@ class User:
     @property
     def verified(self) -> bool:
         """:class:`bool`: Return True if the user is verified account, else False.
-
         .. versionadded: 1.0.0
         """
         return self._payload.get("verified")
@@ -292,7 +252,6 @@ class User:
     @property
     def protected(self) -> bool:
         """:class:`bool`: Return True if the user is protected, else False.
-
         .. versionadded: 1.0.0
         """
         return self._payload.get("protected")
@@ -300,7 +259,6 @@ class User:
     @property
     def avatar_url(self) -> Optional[str]:
         """Optional[:class:`str`]: Return the user profile image.
-
         .. versionadded: 1.0.0
         """
         return self._payload.get("profile_image_url")
@@ -308,7 +266,6 @@ class User:
     @property
     def location(self) -> Optional[str]:
         """:class:`str`: Return the user's location
-
         .. versionadded: 1.0.0
         """
         return self._payload.get("location")
@@ -316,7 +273,6 @@ class User:
     @property
     def created_at(self) -> datetime.datetime:
         """:class:`datetime.datetime`: Return datetime.datetime object with the user's account date.
-
         .. versionadded: 1.0.0
         """
         return time_parse_todt(self._payload.get("created_at"))
@@ -324,7 +280,6 @@ class User:
     @property
     def pinned_tweet(self) -> Optional[object]:
         """Optional[:class:`object`]: Returns the user's pinned tweet.
-
         .. versionadded: 1.1.3
         """
         id = self._payload.get("pinned_tweet_id")
@@ -333,7 +288,6 @@ class User:
     @property
     def followers(self) -> Union[List[User], List]:
         """List[:class:`User`]: Returns a list of users who are followers of the specified user ID. Maximum users is 100 users.
-
         .. versionadded: 1.1.0
         """
         return self._payload.get("followers")
@@ -341,7 +295,6 @@ class User:
     @property
     def following(self) -> Union[List[User], List]:
         """List[:class:`User`]`: Returns a list of users that's followed by the specified user ID. Maximum users is 100 users.
-
         .. versionadded: 1.1.0
         """
         return self._payload.get("following")
@@ -349,7 +302,6 @@ class User:
     @property
     def follower_count(self) -> int:
         """:class:`int`: Return total of followers that a user has.
-
         .. versionadded: 1.1.0
         """
         return self._metrics.follower_count
@@ -357,7 +309,6 @@ class User:
     @property
     def following_count(self) -> int:
         """:class:`int`: Return total of following that a user has.
-
         .. versionadded: 1.1.0
         """
         return self._metrics.following_count
@@ -365,7 +316,6 @@ class User:
     @property
     def tweet_count(self) -> int:
         """:class:`int`: Return total of tweet that a user has.
-
         .. versionadded: 1.1.0
         """
         return self._metrics.tweet_count
@@ -373,7 +323,6 @@ class User:
     @property
     def listed_count(self) -> int:
         """:class:`int`: Return total of listed that a user has.
-
         .. versionadded: 1.1.0
         """
         return self._metrics.listed_count
