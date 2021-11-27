3ec8d2fef47e51c56905a2b86da10b9c22287891
TheKungFuChuck <>
2021-11-27 15:20:17 +0000

    feat: Support 2 legged authentication

--- pytweet/auth.py
+++ pytweet/auth.py
@@ -11,10 +11,11 @@ class OauthSession(OAuth1Session):
     .. versionadded:: 1.2.0
     """
 
-    def __init__(self, consumer_key: str, consumer_secret: str, callback=None) -> None:
+    def __init__(self, consumer_key: Optional[str], consumer_secret: Optional[str], callback=None) -> None:
         super().__init__(consumer_key, client_secret=consumer_secret, callback_uri=callback)
-        self.consumer_key: str = consumer_key
-        self.consumer_key_secret: str = consumer_secret
+        self.is_with_oauth_flow = False
+        self.consumer_key = consumer_key
+        self.consumer_key_secret = consumer_secret
         self.access_token: str = None
         self.access_token_secret: str = None
         self.callback: Any = callback
@@ -24,10 +25,7 @@ class OauthSession(OAuth1Session):
         cls,
         client,
         *,
-        callback: str = "https://twitter.com",
-        auth_access_type: str = "write",
-        force_login: bool = False,
-        screen_name: Optional[str] = None
+        callback: str = "https://twitter.com"
     ):
         """Authorize a user using the 3 legged oauth flow classmethod!
 
@@ -35,12 +33,33 @@ class OauthSession(OAuth1Session):
             This classmethod is not finished yet!
 
         """
-        return 0
-        request_tokens = client.http.request("POST", "", "oauth/request_token", auth=True)
-        oauth_token, oauth_token_secret, oauth_callback_confirmed = request_tokens.split("&")
-        params = {"oauth_token": oauth_token}
+        self = cls(None, None, callback)
+        self.is_with_oauth_flow = True
+        def post_access_token(oauth_token: str, oauth_verifier: str):
+            print(oauth_token, oauth_verifier)
+            res = client.http.request("POST", "", "oauth/access_token", params={"oauth_token": oauth_token, "oauth_verifier": oauth_verifier})
+            oauth_token, oauth_token_secret, user_id, screen_name = res.split("&")
+            return oauth_token, oauth_token_secret, user_id, screen_name
 
-        res = client.http.request("GET", "", "oauth/authorize", params=params, auth=True)
+        def generate_oauth_url(auth_access_type: str = "write"):
+            assert auth_access_type in ["read", "write"]
+            request_tokens = client.http.request(
+                "POST", 
+                "", 
+                "oauth/request_token",
+                params={
+                    "oauth_callback": callback,
+                    "x_auth_access_type": auth_access_type
+                }, 
+                auth=True
+            )
+            oauth_token, oauth_token_secret, oauth_callback_confirmed = request_tokens.split("&")
+            url = "https://api.twitter.com/oauth/authorize" + f"?{oauth_token}"
+            return url
+
+        self.post_access_token = post_access_token 
+        self.generate_oauth_url = generate_oauth_url
+        return self
 
     @property
     def oauth1(self) -> OAuth1:
@@ -48,6 +67,9 @@ class OauthSession(OAuth1Session):
 
         .. versionadded:: 1.2.0
         """
+        if self.is_with_oauth_flow:
+            return None
+
         return OAuth1(
             self.consumer_key,
             client_secret=self.consumer_key_secret,
@@ -59,5 +81,8 @@ class OauthSession(OAuth1Session):
 
     def set_access_token(self, key: str, secret: str) -> None:
         """Set the access token's key and secret."""
+        if self.is_with_oauth_flow:
+            return None
+
         self.access_token = key
         self.access_token_secret = secret
