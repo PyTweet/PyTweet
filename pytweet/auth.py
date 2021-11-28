26e381dec1b636b9ed61ee40a1365b9f8fc26556
Lint Action <lint-action@samuelmeuli.com>
2021-11-28 08:25:03 +0000

    Fix code style issues with Black

--- pytweet/auth.py
+++ pytweet/auth.py
@@ -21,12 +21,7 @@ class OauthSession(OAuth1Session):
         self.callback: Any = callback
 
     @classmethod
-    def with_oauth_flow(
-        cls,
-        client,
-        *,
-        callback: str = "https://twitter.com"
-    ):
+    def with_oauth_flow(cls, client, *, callback: str = "https://twitter.com"):
         """Authorize a user using the 3 legged oauth flow classmethod! This let's your application to do an action on behalf of a user. This will give you 2 new methods, `generate_oauth_url` for generating an oauth url so a user can authorize and `post_oauth_token` posting oauth token and verifier for getting a pair of access token and secret.
 
         .. note::
@@ -38,28 +33,28 @@ class OauthSession(OAuth1Session):
         """
         self = cls(None, None, callback)
         self.is_with_oauth_flow = True
+
         def generate_oauth_url(auth_access_type: str = "write"):
             assert auth_access_type in ["read", "write"]
             request_tokens = client.http.request(
-                "POST", 
-                "", 
+                "POST",
+                "",
                 "oauth/request_token",
-                params={
-                    "oauth_callback": callback,
-                    "x_auth_access_type": auth_access_type
-                }, 
-                auth=True
+                params={"oauth_callback": callback, "x_auth_access_type": auth_access_type},
+                auth=True,
             )
             oauth_token, oauth_token_secret, oauth_callback_confirmed = request_tokens.split("&")
             url = "https://api.twitter.com/oauth/authorize" + f"?{oauth_token}"
             return url
 
         def post_oauth_token(oauth_token: str, oauth_verifier: str):
-            res = client.http.request("POST", "", "oauth/access_token", params={"oauth_token": oauth_token, "oauth_verifier": oauth_verifier})
+            res = client.http.request(
+                "POST", "", "oauth/access_token", params={"oauth_token": oauth_token, "oauth_verifier": oauth_verifier}
+            )
             oauth_token, oauth_token_secret, user_id, screen_name = res.split("&")
             return oauth_token, oauth_token_secret, user_id, screen_name
 
-        self.post_access_token = post_oauth_token 
+        self.post_access_token = post_oauth_token
         self.generate_oauth_url = generate_oauth_url
         return self
 
