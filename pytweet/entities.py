34fdac50508259a508ada2cd969116934af61720
Lint Action <lint-action@samuelmeuli.com>
2021-11-30 06:33:06 +0000

    Fix code style issues with Black

--- pytweet/entities.py
+++ pytweet/entities.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, Tuple, Optional
 
 class Media:
     """Represent media in a message."""
+
     def __init__(self, data: Dict[str, Any]):
         self._payload = data
         self._url = self._payload.get("url")
@@ -35,16 +36,15 @@ class Media:
         """:class:`str`: Returns the image's height"""
         return int(self._height)
 
-    
-
 
 class Hashtags:
     """Represent hashtags in a message"""
+
     def __init__(self, data=Dict[str, Any]):
         self._payload = data
         self._text: Optional[str] = self._payload.get("text")
         self._startpoint, self._endpoint = self._payload.get("indices")
-    
+
     @property
     def text(self) -> Optional[str]:
         """Optional[:class:`str`]: Returns the hashtag's text.OSError"""
@@ -56,9 +56,9 @@ class Hashtags:
         return self._startpoint, self._endpoint
 
 
-
 class UserMentions:
     """Represent user mention in a message."""
+
     def __init__(self, data=Dict[str, Any]):
         self._payload: Dict[str, Any] = data
         self._name: str = self._payload.get("name")
@@ -89,6 +89,7 @@ class UserMentions:
 
 class Urls:
     """Represent Urls in a message."""
+
     def __init__(self, data=Dict[str, Any]):
         self._payload: Dict[str, Any] = data
         self._url: str = self._payload.get("url")
@@ -117,9 +118,9 @@ class Urls:
         return self._startpoint, self._endpoint
 
 
-
 class Symbols:
     """Represent Symbols in a message."""
+
     def __init__(self, data=Optional[Dict[str, Any]]):
         self._payload: Dict[str, Any] = data
         self._text: str = self._payload.get("text")
