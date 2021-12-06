82a1ab37f40121bce531a0458071dd009a21f0c5
TheKungFuChuck <>
2021-12-06 15:29:09 +0000

    feat: Add 2 parsers class

---
+++ pytweet/parsers.py
@@ -0,0 +1,44 @@
+from typing import Dict, Any
+from .message import DirectMessage
+from .user import User
+
+class PayloadParser:
+    def parse_user_payload(self, payload: Dict[str, Any]):
+        copy = payload.copy()
+        copy["public_metrics"] = {
+            "followers_count": copy.get("followers_count"),
+            "following_count": copy.get("friends_count"),
+            "tweet_count": copy.get("statuses_count"),
+            "listed_count": 0 #
+        }
+        if "created_timestamp" in copy.keys():
+            copy["created_at"] = copy.get("created_timestamp")
+        if "screen_name" in copy.keys():
+            copy["username"] = copy.get('screen_name')
+            
+        if "profile_image_url" in copy.keys():
+            copy["profile_image_url"] = copy.get("profile_image_url_https")
+        return copy
+        
+
+class EventParser:
+    def __init__(self, http_client: object):
+        self.http_client = http_client
+        self.payload_parser = PayloadParser()
+
+    def parse_direct_message_create(self, direct_message_payload: Dict[str, Any]):
+        event_payload = {"event": direct_message_payload.get("direct_message_events")[0]}
+        users = direct_message_payload.get("users")
+
+        message_create = event_payload["event"].get("message_create")
+        recipient_id = message_create.get("target").get("recipient_id")
+        sender_id = message_create.get("sender_id")
+        recipient = User(self.payload_parser.parse_user_payload(users.get(recipient_id)), http_client=self.http_client)
+        sender = User(self.payload_parser.parse_user_payload(users.get(sender_id)), http_client=self.http_client)
+
+        event_payload["event"]["message_create"]["target"]["recipient"] = recipient
+        event_payload["event"]["message_create"]["target"]["sender"] = sender
+
+        direct_message = DirectMessage(event_payload, http_client=self.http_client)
+        self.http_client.message_cache[direct_message.id] = direct_message
+        self.http_client.dispatch("direct_message", direct_message)
