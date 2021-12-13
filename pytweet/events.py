cee9e3eea42a282112b7b9e330c023f6604270c0
TheKungFuChuck <>
2021-12-13 14:47:39 +0000

    feat: Implement more event objects

--- pytweet/events.py
+++ pytweet/events.py
@@ -3,6 +3,49 @@ from typing import Any, Dict, Optional
 
 from .user import User
 
+class UserActionEvent:
+    """Represents a user action type event, this could be but not limited to follow or unfollow. This also a parent class to all UserAction events.
+    
+    .. versionadded:: 1.3.5
+    """
+    def __init__(self, data: Dict[str, Any]):
+        self.original_payload = data
+        self._payload = data.get("follow_events")[0]
+
+    @property
+    def created_at(self) -> datetime.datetime:
+        """:class:`datetime.datetime`: Returns a datetime.datetime object with the action's created timestamp.
+        
+        .. versionadded:: 1.5.0
+        """
+        timestamp = str(self._payload.get("created_timestamp"))[:10]
+        return datetime.datetime.fromtimestamp(int(timestamp))
+
+    @property
+    def target(self) -> User:
+        """:class:`User`: Returns the user who got followed/unfollowed by the source.
+        
+        .. versionadded:: 1.5.0
+        """
+        return self._payload.get("target")
+
+    @property
+    def source(self) -> User:
+        """:class:`User`: Returns a user object that trigger this event.
+        
+        .. versionadded:: 1.5.0
+        """
+        return self._payload.get("source")
+
+    @property
+    def author(self):
+        """:class:`User`: An alias to source.
+        
+        .. versionadded:: 1.5.0
+        """
+        return self.source
+
+
 class DirectMessageTypingEvent:
     """Represents a typing event object for `on_typing` event. This object contains the event information that twitter posts through the webhook url. 
     
@@ -91,51 +134,46 @@ class DirectMessageReadEvent:
         """
         return self._payload.get("last_read_event_id")
 
-class UserFollowActionEvent:
-    """Represents a follow action event. This object contains information that twitter posts through the webhook url.
+class UserFollowActionEvent(UserActionEvent):
+    """Represents a follow action event by user or of user, This inherits :class:`UserActionEvent`. This object contains information that twitter posts through the webhook url.
     
     .. versionadded:: 1.5.0
     """
-    def __init__(self, data: Dict[str, Any]):
-        self.original_payload = data
-        self._payload = data.get("follow_events")[0]
-
-    @property
-    def created_at(self) -> datetime.datetime:
-        """:class:`datetime.datetime`: Returns a datetime.datetime object with the action's created timestamp.
-        
-        .. versionadded:: 1.5.0
-        """
-        timestamp = str(self._payload.get("created_timestamp"))[:10]
-        return datetime.datetime.fromtimestamp(int(timestamp))
-
-    @property
-    def target(self) -> User:
-        """:class:`User`: Returns the user who got followed/unfollowed by the source.
-        
-        .. versionadded:: 1.5.0
-        """
-        return self._payload.get("target")
-
-    @property
-    def source(self) -> User:
-        """:class:`User`: Returns a user object that trigger this event.
-        
-        .. versionadded:: 1.5.0
-        """
-        return self._payload.get("source")
-
-    @property
-    def author(self):
-        """:class:`User`: An alias to source.
-        
-        .. versionadded:: 1.5.0
-        """
-        return self.source
+    pass
 
 
-class UserUnfollowActionEvent(UserFollowActionEvent):
-    """Represents an unfollow action event, This inherits :class:`UserFollowActionEvent`. This object contains information that twitter posts through the webhook url.
+class UserUnfollowActionEvent(UserActionEvent):
+    """Represents an unfollow action event by user, This inherits :class:`UserActionEvent`. This object contains information that twitter posts through the webhook url.
+    
+    .. versionadded:: 1.5.0
+    """
+    pass
+
+#TODO Dispatch the events and return these objects.
+
+class UserBlockActionEvent(UserActionEvent):
+    """Represents a block action event by user, This inherits :class:`UserActionEvent`. This object contains information that twitter posts through the webhook url.
+    
+    .. versionadded:: 1.5.0
+    """
+    pass
+
+class UserUnblockActionEvent(UserActionEvent):
+    """Represents an unblock action event by user, This inherits :class:`UserActionEvent`. This object contains information that twitter posts through the webhook url.
+    
+    .. versionadded:: 1.5.0
+    """
+    pass
+
+class UserMuteActionEvent(UserActionEvent):
+    """Represents a mute action event by user, This inherits :class:`UserActionEvent`. This object contains information that twitter posts through the webhook url.
+    
+    .. versionadded:: 1.5.0
+    """
+    pass
+
+class UserUnmuteActionEvent(UserActionEvent):
+    """Represents an unmute action event by user, This inherits :class:`UserActionEvent`. This object contains information that twitter posts through the webhook url.
     
     .. versionadded:: 1.5.0
     """
