dd3dffa5ebd5fdc84478ce709e45ade28d5803f4
TheKungFuChuck <>
2021-12-24 13:07:06 +0000

    feat: Add a new class for Space

--- pytweet/dataclass/__init__.py
+++ pytweet/dataclass/__init__.py
@@ -1,4 +1,5 @@
 from .attachments import *
 from .locations import *
 from .settings import *
+from .space import *
 from .stream import *
---
+++ pytweet/dataclass/space.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
+class SpaceTopic:
+    id: int
+    name: str
+    description: str
