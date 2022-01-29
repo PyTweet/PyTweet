feed02b3a1d7feeb262755fb79b2f3d84b265e2a
TheKungFuChuck <>
2022-01-29 04:27:36 +0000

    chore: Add list fields and expansion

--- pytweet/constants.py
+++ pytweet/constants.py
@@ -1,6 +1,7 @@
-# Expansions & Fields use for extend object.
+# Expansions & Fields use for extend object data.
 TWEET_EXPANSION = "attachments.poll_ids,attachments.media_keys,author_id,geo.place_id,in_reply_to_user_id,referenced_tweets.id,entities.mentions.username,referenced_tweets.id.author_id"
 SPACE_EXPANSION = "invited_user_ids,speaker_ids,creator_id,host_ids"
+LIST_EXPANSION = "owner_id"
 TWEET_FIELD = "attachments,author_id,context_annotations,conversation_id,created_at,geo,entities,in_reply_to_user_id,lang,possibly_sensitive,public_metrics,referenced_tweets,reply_settings,source,text,withheld"
 USER_FIELD = "created_at,description,entities,id,location,name,profile_image_url,protected,public_metrics,url,username,verified,withheld,pinned_tweet_id"
 SPACE_FIELD = "host_ids,created_at,creator_id,id,lang,invited_user_ids,participant_count,speaker_ids,started_at,state,title,updated_at,scheduled_start,is_ticketed"
@@ -8,13 +9,14 @@ MEDIA_FIELD = "duration_ms,height,media_key,preview_image_url,public_metrics,typ
 PLACE_FIELD = "contained_within,country,country_code,full_name,geo,id,name,place_type"
 POLL_FIELD = "duration_minutes,end_datetime,id,options,voting_status"
 TOPIC_FIELD = "id,name,description"
+LIST_FIELD = "created_at,follower_count,member_count,private,description,owner_id"
 
-# Indicator passed in return_when in wait_for_futures module function.
+# Indicator for the return_when argument in wait_for_futures method.
 FIRST_COMPLETED = "FIRST_COMPLETED"
 FIRST_EXCEPTION = "FIRST_EXCEPTION"
 ALL_COMPLETED = "ALL_COMPLETED"
 
-# Language codes for subtitle.
+# Language codes for subtitle that based on BCP47 style.
 LANGUAGES_CODES = {
     "ar-SA": "Arabic",
     "bn-BD": "Bangla",
