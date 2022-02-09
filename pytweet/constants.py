a81f205b5fbb7a66fd48d221cad15b4ae2713002
TheKungFuChuck <>
2022-02-09 14:29:40 +0000

    chore: Add a new constant

--- pytweet/constants.py
+++ pytweet/constants.py
@@ -2,6 +2,8 @@
 TWEET_EXPANSION = "attachments.poll_ids,attachments.media_keys,author_id,geo.place_id,in_reply_to_user_id,referenced_tweets.id,entities.mentions.username,referenced_tweets.id.author_id"
 SPACE_EXPANSION = "invited_user_ids,speaker_ids,creator_id,host_ids"
 LIST_EXPANSION = "owner_id"
+PINNED_TWEET_EXPANSION = "pinned_tweet_id"
+
 TWEET_FIELD = "attachments,author_id,context_annotations,conversation_id,created_at,geo,entities,in_reply_to_user_id,lang,possibly_sensitive,public_metrics,referenced_tweets,reply_settings,source,text,withheld"
 USER_FIELD = "created_at,description,entities,id,location,name,profile_image_url,protected,public_metrics,url,username,verified,withheld,pinned_tweet_id"
 SPACE_FIELD = "host_ids,created_at,creator_id,id,lang,invited_user_ids,participant_count,speaker_ids,started_at,state,title,updated_at,scheduled_start,is_ticketed"
