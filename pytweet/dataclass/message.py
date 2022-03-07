d187625e6fd5a5c2936a9817f8bb9819ab81caff
TheKungFuChuck <>
2022-03-07 13:39:52 +0000

    chore: Add a new Initiated object

---
+++ pytweet/dataclass/message.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from ..type import ID
+
+@dataclass
+class Initiated:
+    """Represents an object that stores 'initiated_via' key from direct message event data.
+
+    .. versionadded:: 1.5.0
+    """
+    tweet_id: ID
+    welcome_message_id: ID
